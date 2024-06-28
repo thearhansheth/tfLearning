763ab76cf8ca442e480353b4815af79fa7f3cf43
thearhansheth <arhan.sheth@gmail.com>
2024-06-28 14:42:52 +0530

    Added dimension for prediction

--- mobilenet_v2_imageClassication.py
+++ mobilenet_v2_imageClassication.py
@@ -9,9 +9,16 @@ import PIL.Image as Image
 from tensorflow.python.keras import layers
 from tensorflow.python.keras import Sequential
 
+#pre-determining image input shape
 IMAGE_SHAPE = (224, 224)
+
+# initializing classifier model
 classifier = Sequential([hub.KerasLayer("https://www.kaggle.com/models/google/mobilenet-v2/TensorFlow2/035-128-classification/2", input_shape = IMAGE_SHAPE + (3,))])
 
+# initialzing and pre-processing the sample input image
 goldfish_img = Image.open("/Users/arhan.sheth/Documents/Codes/DX/transferLearning/tfLearning/goldfish.png").resize(IMAGE_SHAPE)
 goldfish_img = np.array(goldfish_img)/255.0
-print(goldfish_img.shape)
+
+# adding another dimension for prediction 
+goldfish_img = np.expand_dims(goldfish_img, axis=0)
+res = classifier.predict(goldfish_img)
