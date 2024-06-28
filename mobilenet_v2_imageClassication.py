6ac969619de1202c751c5ee08d3060249e291ab5
thearhansheth <arhan.sheth@gmail.com>
2024-06-28 15:59:55 +0530

    Keras resolved and initial prediction made

--- mobilenet_v2_imageClassication.py
+++ mobilenet_v2_imageClassication.py
@@ -3,22 +3,28 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow_hub as hub
-import cv2 as cv
-import PIL.Image as Image
+from PIL import Image
 
-from tensorflow.python.keras import layers
-from tensorflow.python.keras import Sequential
+from keras import layers, models, losses, optimizers
+from keras import Sequential
 
-#pre-determining image input shape
+# Pre-determining image input shape
 IMAGE_SHAPE = (224, 224)
 
-# initializing classifier model
-classifier = Sequential([hub.KerasLayer("https://www.kaggle.com/models/google/mobilenet-v2/TensorFlow2/035-128-classification/2", input_shape = IMAGE_SHAPE + (3,))])
+# Initializing classifier model with a proper TensorFlow Hub URL
+classifier = models.Sequential([
+    hub.KerasLayer("https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/4", input_shape=IMAGE_SHAPE + (3,))
+    ])
 
-# initialzing and pre-processing the sample input image
-goldfish_img = Image.open("/Users/arhan.sheth/Documents/Codes/DX/transferLearning/tfLearning/goldfish.png").resize(IMAGE_SHAPE)
-goldfish_img = np.array(goldfish_img)/255.0
+# Initializing and pre-processing the sample input image
+image_path = "/Users/arhan.sheth/Documents/Codes/DX/transferLearning/tfLearning/goldfish.png"
+goldfish_img = Image.open(image_path).resize(IMAGE_SHAPE)
+goldfish_img = np.array(goldfish_img) / 255.0
 
-# adding another dimension for prediction 
+# Adding another dimension for prediction
 goldfish_img = np.expand_dims(goldfish_img, axis=0)
 res = classifier.predict(goldfish_img)
+
+# Output the prediction result
+predicted_index = np.argmax(res)
+print(predicted_index)
